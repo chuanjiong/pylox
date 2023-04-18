95a335815713da6161cab0683f27d8849f77d4b2
laichuanjiong <361038903@qq.com>
2023-04-18 16:15:18 +0800

    修改my_lox流程

--- my_lox.py
+++ my_lox.py
@@ -6,43 +6,45 @@ from my_parser import Parser
 from my_resolver import Resolver
 from my_env import Env
 
-#my_env.global_env.define(Token(TokenType.IDENTIFIER, 'clock', None, 0), my_type.ClockFunc('clock', 0))
-
 def run(src, resolver, env):
-    try:
-        value = None
-        statements = Parser(Scanner(src, env).scan_tokens(), env).parse()
-        for statement in statements:
-            print(f'{statement}')
-        for statement in statements:
-            value = statement.resolve(resolver)
-        for statement in statements:
-            value = statement.exec(env)
-        return value
-    except KeyboardInterrupt:
-        sys.exit()
+    value = None
+    statements = Parser(Scanner(src, env).scan_tokens(), env).parse()
+    print('+'*16 + ' ast ' + '+'*16)
+    for statement in statements:
+        print(f'{statement}')
+    print('-'*16 + ' ast ' + '-'*16)
+    print('+'*16 + ' exec ' + '+'*16)
+    for statement in statements:
+        statement.resolve(resolver)
+    for statement in statements:
+        value = statement.exec(env)
+    print('-'*16 + ' exec ' + '-'*16)
+    return value
 
-def runFile(path):
-    resolver = Resolver()
-    env = Env()
+def run_file(path):
+    print('='*16 + f' run: {path} ' + '='*16)
     with open(path, 'r') as f:
-        run(f.read(), resolver, env)
+        run(f.read(), Resolver(), Env())
 
-def runPrompt():
+def run_prompt():
     resolver = Resolver()
     env = Env()
     while True:
-        print('> ', end = '')
-        value = run(input(), resolver, env)
-        if value is not None:
-            print(f'{value}')
+        print('> ', end='')
+        try:
+            value = run(input(), resolver, env)
+            if value is not None:
+                print(f'{value}')
+        except KeyboardInterrupt:
+            sys.exit()
+        except:
+            pass
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        print(f'Usage: {sys.argv[0]} [script]')
-    elif len(sys.argv) == 2:
-        runFile(sys.argv[1])
+    if len(sys.argv) == 1:
+        run_prompt()
     else:
-        runPrompt()
+        for i in range(1, len(sys.argv)):
+            run_file(sys.argv[i])
 
